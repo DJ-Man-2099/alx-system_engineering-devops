5b37c9a74095d91c7b23a03ae68472889da7edbb
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-28 19:57:28 +0200

    Optional Task 1

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[A-Z]/).join
+SENDER = ARGV[0].scan(/\[from:(.+?)\]/).join
+RECEIVER = ARGV[0].scan(/\[to:(.+?)\]/).join
+FLAGS = ARGV[0].scan(/\[flags:(.+?)\]/).join
+puts SENDER+","+RECEIVER+","+FLAGS
