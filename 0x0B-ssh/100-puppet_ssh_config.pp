28a1f3c9cf8760f796e2546120b03f421115005d
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-20 20:44:26 +0200

    Optional Task 1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,30 @@
 # append a Host to ssh_config configuration file
-
-file {
-  'create config':
-  path    => '/home/config',
-  ensure  => file,
-  content => 'Host "18.207.142.187"
-    HostName "18.207.142.187"
-    IdentityFile "~/.ssh/school"
-    BatchMode yes
-    IdentitiesOnly yes
-    PasswordAuthentication no
-',
-  group   => 'root',
-  owner   => 'root'
+file_line{'Turn off passwd auth':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
 }
 
-file_line { 'add config to ssh_config':
-  path => '/etc/ssh/ssh_config',
-  line => 'Include /home/config',
-  ensure => present,
+file_line{'Declare identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
 }
+# file {
+#   'create config':
+#   path    => '/home/config',
+#   ensure  => file,
+#   content => 'Host "18.207.142.187"
+#     HostName "18.207.142.187"
+#     IdentityFile "~/.ssh/school"
+#     BatchMode yes
+#     IdentitiesOnly yes
+#     PasswordAuthentication no
+# ',
+#   group   => 'root',
+#   owner   => 'root'
+# }
+
+# file_line { 'add config to ssh_config':
+#   path => '/etc/ssh/ssh_config',
+#   line => 'Include /home/config',
+#   ensure => present,
+# }
