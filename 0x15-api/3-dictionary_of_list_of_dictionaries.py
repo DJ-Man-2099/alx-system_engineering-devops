a2b3be910fb7e40d423d3bd003e37a29f1d1475f
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-19 20:11:32 +0200

    Task 4

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,32 +10,35 @@ import sys
 API_URL = "https://jsonplaceholder.typicode.com/"
 
 
-def get_employee(id):
+def get_all_employees():
     """
-    returns information about his/her TODO list progress
+    returns information about all TODO list progress
     """
-    if not isinstance(id, int):
-        raise ValueError("id must be an integer")
-    if id is None:
-        raise ValueError("id must be supplied")
-    user_url = str.format("users/{}", id)
-    user = requests.get(API_URL+user_url).json()
-    todos_url = str.format(
-        "users/{}/todos", id)
+    users_url = "users"
+    users = requests.get(API_URL+users_url).json()
+    todos_url = "todos"
     todos = requests.get(API_URL+todos_url).json()
     dict = {}
-    dict[id] = []
-    username = user.get("username")
-    for todo in todos:
-        dict[id].append(
-            {"task": todo.get("title"),
-             "completed": todo.get("completed"),
-             "username": username, }
+    for user in users:
+        id = user.get("id")
+        dict[id] = []
+        username = user.get("username")
+        user_todos = list(
+            filter(
+                lambda x: x.get("userId") == id,
+                todos
+            )
         )
+        for todo in user_todos:
+            dict[id].append(
+                {"task": todo.get("title"),
+                 "completed": todo.get("completed"),
+                 "username": username, }
+            )
     with open(str.format("{}.json", id), 'w') as json_file:
         json.dump(dict, json_file)
 
 
 if __name__ == "__main__":
     id = int(sys.argv[1])
-    get_employee(id)
+    get_all_employees(id)
