b0db2c4b9752b3a5afd3d7d63a277754c972c9eb
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-19 19:57:57 +0200

    Task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -24,7 +24,7 @@ def get_employee(id):
         "users/{}/todos", id)
     todos = requests.get(API_URL+todos_url).json()
     rows = []
-    username = user.get("username")[0],
+    username = user.get("username")
     for todo in todos:
         rows.append([
             id,
