5d4d4a1a0af200583ca24bb7f1a6c1b17bda550c
DJ-Man-2099 <davidmesak@hotmail.com>
2024-03-05 21:09:48 +0200

    Task 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API
+and returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+BASE = 'https://www.reddit.com/r/'
+
+
+def top_ten(subreddit):
+    """function"""
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    data = requests.get(BASE + str.format("{}/hot.json?limit=10",
+                        subreddit), headers=headers,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print('None')
+        return
+    posts = data.json().get('data').get('children')
+    for post in posts:
+        print(post.get('data').get('title'))
