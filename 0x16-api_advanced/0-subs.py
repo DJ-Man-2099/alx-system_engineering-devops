665334af8cc91f66ab09a49d6c9321e6db9b5499
DJ-Man-2099 <davidmesak@hotmail.com>
2024-03-05 20:34:52 +0200

    Task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API
+and returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+BASE = 'https://www.reddit.com/r/'
+
+
+def number_of_subscribers(subreddit):
+    """function"""
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    data = requests.get(BASE + str.format("{}/about.json",
+                        subreddit), headers=headers).json()
+    if 'error' in data:
+        return 0
+    return data['data']['subscribers']
