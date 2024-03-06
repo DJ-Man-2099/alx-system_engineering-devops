0c01457dd7f4a8edf4aa74f5e256e512b614b2d1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-03-06 08:09:05 +0200

    Task 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,18 @@ Queries the Reddit API
 and returns the number of subscribers for a given subreddit
 """
 
+import base64
 import requests
+from urllib.parse import quote
 
 BASE = 'https://www.reddit.com/r/'
-
+CLIENT_ID = "gjV8WEVc3awcbN00hZLVpdD6gbo3_A"
 
 def top_ten(subreddit):
     """function"""
-    headers = {'User-Agent': 'Custom22099'}
+    headers = {'User-Agent': 'my-app/0.0.1/220942',
+     'Authorization':
+        "Basic " + base64.b64encode((quote(CLIENT_ID) + ":").encode()).decode(),}
     data = requests.get(BASE + str.format("{}/hot.json?limit=10",
                         subreddit), headers=headers,
                         allow_redirects=False)
