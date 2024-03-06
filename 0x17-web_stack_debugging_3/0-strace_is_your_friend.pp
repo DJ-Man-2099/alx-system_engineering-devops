00555d8f98c86358fbcdb1e1a75c42d12e56fc43
DJ-Man-2099 <davidmesak@hotmail.com>
2024-03-06 23:42:26 +0200

    TASk 1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,9 @@
 # Fix apache error
+# Change import in wp-settings.php file
+# from require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );
+# to require_once( ABSPATH . WPINC . '/class-wp-locale.php' );
 exec { 'install-puppetlabs-stdlib':
-  command => 'puppet module install puppetlabs-stdlib',
+  command => 'puppet module install puppetlabs-stdlib --version 4.25.1 --force',
   path    => ['/usr/bin', '/usr/sbin'],
 }
 file_line { 'fix-wordpress':
